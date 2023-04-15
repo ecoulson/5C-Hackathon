704a8adcd672917b8c7049c929da5e24f43f0275
tobyfrank <tfrank@hmc.edu>
2023-04-14 23:55:26 -0700

    doc version

--- backend/src/img_to_text.py
+++ backend/src/img_to_text.py
@@ -29,3 +29,31 @@ def detect_text(path: str, output_file: str):
                 response.error.message))
     
     return paragraph
+
+
+def pic_to_text(infile):
+    """Detects text in an image file
+
+    ARGS
+    infile: path to image file
+
+    RETURNS
+    String of text detected in image
+    """
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # Opens the input image file
+    with io.open(infile, "rb") as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # For dense text, use document_text_detection
+    # For less dense text, use text_detection
+    response = client.document_text_detection(image=image)
+    text = response.full_text_annotation.text
+    print("Detected text: {}".format(text))
+
+    return text
--- backend/src/img_to_text_test.py
+++ backend/src/img_to_text_test.py
@@ -1,17 +1,18 @@
 import unittest
-from img_to_text import detect_text
+from img_to_text import detect_text, pic_to_text
 
-class TestImgToText(unittest.TestCase):
+class TestImgToText(unittest.TestCase):        
 
-    def test_detect_text(self):
+    def test_pic_to_text(self):
         expected_text = 'OFFICIAL BALLOT'
 
-        actual_text = detect_text('/Users/tobyfrank/Desktop/'
+        actual_text = pic_to_text('/Users/tobyfrank/Desktop/'
                                   'Spring 2023/5C-Hackathon/'
                                   'backend/src/ballot_imgs/b'
-                                  'allot_test1.png', "ballot_text.txt")
-        print("actual_text", actual_text)
+                                  'allot_test1.png')
+        
         self.assertTrue(expected_text in actual_text)
+    
 
 if __name__ == '__main__':
     unittest.main()
