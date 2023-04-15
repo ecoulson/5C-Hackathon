edf2242eb9d00a6c8f51c3b9cde0d07eb732d62e
Amy Tam <amytam01@gmail.com>
2023-04-15 01:24:16 -0700

    new test

--- backend/src/img_to_text_test.py
+++ backend/src/img_to_text_test.py
@@ -9,8 +9,20 @@ class TestImgToText(unittest.TestCase):
         actual_text = pic_to_text('/Users/tobyfrank/Desktop/'
                                   'Spring 2023/5C-Hackathon/'
                                   'backend/src/ballot_imgs/b'
-                                  'allot_test1.png', 'ballot_text.txt')
+                                  'allot_test1.png', 'ballot_1_text.txt')
         
+        self.assertTrue(expected_text in actual_text)
+
+    def test_pic_to_text(self):
+        expected_text = 'Demonstration General Election'
+
+        actual_text = pic_to_text(('/Users/amytam/Desktop/'
+                                  'HMC_2023_Spring/5C-Hac'
+                                  'kathon/backend/src/bal'
+                                  'lot_imgs/ballot_test3.'
+                                  'jpeg'), 'ballot_3_text.txt')
+        
+        print(actual_text)
         self.assertTrue(expected_text in actual_text)
     
 
